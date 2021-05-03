d40a1c3af0e108ded480107b312ae80f29debf66
wesleydeivid23@hotmail.com <wesleydeivid23@gmail.com>
2021-05-03 15:12:19 -0300

    Programa simples aluguel de quarto com vetores

---
+++ Aluguel_quarto/Estudante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aluguel_quarto
+{
+    class Estudante
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Name + "," + Email;
+        }
+    }
+}
---
+++ Aluguel_quarto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aluguel_quarto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("How many room will be rented: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"rent#{i}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("email: ");
+                string email = Console.ReadLine();
+                Console.Write("room: ");
+                int room = int.Parse(Console.ReadLine());
+                vect[room] = new Estudante(name, email);
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("rented rooms");
+            for (int i = 0; i < 10; i++) {
+                if (vect[i] != null) {
+                    Console.WriteLine(i +": " + vect[i]);
+                }
+            }
+        }
+    }
+}
